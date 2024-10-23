464d99b6f98ec6378520daef6378f1328daf43a2
~venir <luca@venir.dev>
2024-10-23 11:38:03 +0200

    tests
    not in our scope, but still...

--- test/cross_platform_its_aapn_2024_test.dart
+++ test/cross_platform_its_aapn_2024_test.dart
@@ -2,7 +2,7 @@ import 'package:cross_platform_its_aapn_2024/cross_platform_its_aapn_2024.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
+  test('calculate works as intended', () {
     expect(calculate(), 42);
   });
 }
