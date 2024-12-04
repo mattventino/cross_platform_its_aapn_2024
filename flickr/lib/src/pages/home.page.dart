1dbae6ad06293513d9938d768493ab899ad68083
~venir <luca@venir.dev>
2024-12-04 17:13:21 +0100

    UI implemented

--- flickr/lib/src/pages/home.page.dart
+++ flickr/lib/src/pages/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:flickr/src/providers/products.provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +7,34 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final products = ref.watch(productsProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: switch (products) {
+            AsyncData(:final value) => GridView.count(
+                crossAxisCount: 3,
+                children: [
+                  for (final product in value)
+                    Card(
+                      margin: const EdgeInsets.all(32),
+                      child: Column(
+                        children: [
+                          Text(product.title),
+                          Image.network(product.thumbnail),
+                          Text(product.description)
+                        ],
+                      ),
+                    )
+                ],
+              ), // TODO later
+            AsyncError() => const Text("uh-oh, è andato tutto a quel paese..."),
+            _ => const CircularProgressIndicator()
+          },
+        ),
+      ),
+    );
   }
 }
