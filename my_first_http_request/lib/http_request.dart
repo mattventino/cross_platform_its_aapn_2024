517cd25cc37aa52f5b12d89c137714aaa4ad3a7c
~venir <luca@venir.dev>
2024-12-04 12:11:17 +0100

    with `Dio`

--- my_first_http_request/lib/http_request.dart
+++ my_first_http_request/lib/http_request.dart
@@ -1,22 +1,20 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:my_first_http_request/faker_response.model.dart';
 
 Future<FakerResponseApiModel> fetchProducts() async {
+  final client = Dio(
+    BaseOptions(
+        receiveTimeout: Duration(seconds: 3),
+        baseUrl: "https://fakerapi.it/api/v2"),
+  );
+
   // https://fakerapi.it/api/v2/products?_quantity=5&_price_min=20
-  final url = Uri.https('fakerapi.it', 'api/v2/products', {
-    "_quantity": "5",
-    "_price_min": "20",
-    "_price_max": "100",
-  });
-  final response = await http.get(url);
-  print('Response status: ${response.statusCode}');
+  try {
+    final response = await client.get<Map<String, dynamic>>("/products");
+    print('Response status: ${response.statusCode}');
 
-  final decoded = jsonDecode(response.body);
-  final map = decoded as Map<String, Object?>;
-
-  final result = FakerResponseApiModel.fromJson(map);
-
-  return result;
+    return FakerResponseApiModel.fromJson(response.data!);
+  } finally {
+    client.close();
+  }
 }
