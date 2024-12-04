3e2db622b38439615fe59a96edeba77df7712a39
~venir <luca@venir.dev>
2024-12-04 12:17:21 +0100

    reset

--- my_first_http_request/bin/my_first_http_request.dart
+++
@@ -1,9 +0,0 @@
-import 'package:my_first_http_request/http_request.dart';
-
-void main(List<String> arguments) async {
-  final body = await fetchProducts();
-  // print('Response body:');
-  // print(body);
-
-  print(body.data.first);
-}
--- my_first_http_request/test/my_first_http_request_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:my_first_http_request/my_first_http_request.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
