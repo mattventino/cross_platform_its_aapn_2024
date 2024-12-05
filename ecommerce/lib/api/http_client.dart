1ed402d460db690a8a9735c1b40447b5679286a8
~venir <luca@venir.dev>
2024-12-05 10:49:28 +0100

    created httpclient

---
+++ ecommerce/lib/api/http_client.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'http_client.g.dart';
+
+@riverpod
+Dio httpClient(HttpClientRef ref) {
+  ref.keepAlive();
+  final client = Dio(
+    BaseOptions(baseUrl: 'https://dummyjson.com'),
+  );
+  ref.onDispose(client.close);
+
+  return client;
+}
---
+++ ecommerce/lib/api/http_client.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'http_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$httpClientHash() => r'bec93af7857b1d2bf6b2b69f16ab6dbd2038b2ed';
+
+/// See also [httpClient].
+@ProviderFor(httpClient)
+final httpClientProvider = AutoDisposeProvider<Dio>.internal(
+  httpClient,
+  name: r'httpClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$httpClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef HttpClientRef = AutoDisposeProviderRef<Dio>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
