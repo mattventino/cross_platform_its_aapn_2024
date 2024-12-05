1d267da7b38f8436bf893fbb7ce114746c090c9b
~venir <luca@venir.dev>
2024-12-05 14:37:34 +0100

    home page styling

--- ecommerce/lib/src/pages/home.page.dart
+++ ecommerce/lib/src/pages/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/src/providers/cart.provider.dart';
 import 'package:ecommerce/src/providers/products.provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,6 +9,7 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final products = ref.watch(productsProvider);
+    final theme = Theme.of(context);
 
     return Scaffold(
       body: Center(
@@ -17,14 +19,45 @@ class HomePage extends ConsumerWidget {
             AsyncData(:final value) => ListView(
                 children: [
                   for (final product in value)
-                    Card(
-                      margin: const EdgeInsets.symmetric(vertical: 16),
-                      child: Column(
-                        children: [
-                          Text(product.title),
-                          Image.network(product.thumbnail),
-                          Text(product.description),
-                        ],
+                    Center(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxWidth: 96 * 5,
+                        ),
+                        child: Card(
+                          margin: const EdgeInsets.symmetric(vertical: 16),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 20,
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  product.title,
+                                  style: theme.textTheme.titleLarge?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Image.network(product.thumbnail),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 16,
+                                  ),
+                                  child: Text(product.description),
+                                ),
+                                const SizedBox(height: 36),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    ref
+                                        .read(cartProvider.notifier)
+                                        .addToCart(product);
+                                  },
+                                  child: const Text("Aggiungi"),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     )
                 ],
